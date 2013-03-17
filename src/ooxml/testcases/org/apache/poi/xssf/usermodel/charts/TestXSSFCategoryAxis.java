13db1e7827ded8c41dfa2a09cd3f7e72ab7cbe56
Martin Andersson <martin.andersson@purplescout.se>
2013-03-17 10:08:17 +0100

    New chart axis type: Category axis
    https://issues.apache.org/bugzilla/show_bug.cgi?id=54676

--- src/ooxml/testcases/org/apache/poi/xssf/usermodel/charts/TestXSSFCategoryAxis.java
+++ src/ooxml/testcases/org/apache/poi/xssf/usermodel/charts/TestXSSFCategoryAxis.java
@@ -15,26 +15,26 @@
    limitations under the License.
 ==================================================================== */
 
-package org.apache.poi.ss.usermodel.charts;
+package org.apache.poi.xssf.usermodel.charts;
 
-import org.apache.poi.util.Beta;
+import junit.framework.TestCase;
 
-/**
- * A factory for different chart axis.
- *
- * @author Roman Kashitsyn
- */
-@Beta
-public interface ChartAxisFactory {
+import org.apache.poi.ss.usermodel.charts.*;
+import org.apache.poi.xssf.usermodel.*;
 
-	/**
-	 * @return new value axis
-	 */
-	ValueAxis createValueAxis(AxisPosition pos);
+public final class TestXSSFCategoryAxis extends TestCase {
 
-	/**
-	 * @return new category axis.
-	 */
-	ChartAxis createCategoryAxis(AxisPosition pos);
+	public void testAccessMethods() throws Exception {
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFSheet sheet = wb.createSheet();
+		XSSFDrawing drawing = sheet.createDrawingPatriarch();
+		XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 1, 1, 10, 30);
+		XSSFChart chart = drawing.createChart(anchor);
+		XSSFCategoryAxis axis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
 
+		axis.setCrosses(AxisCrosses.AUTO_ZERO);
+		assertEquals(axis.getCrosses(), AxisCrosses.AUTO_ZERO);
+
+		assertEquals(chart.getAxis().size(), 1);
+	}
 }
