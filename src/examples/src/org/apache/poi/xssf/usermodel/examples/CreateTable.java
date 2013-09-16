a550a904452adfff94a310642a8c091245a718c6
karinsofia <karinsofiapaulina@gmail.com>
2013-09-16 11:40:12 +0200

    Create workbook with empty table

--- src/examples/src/org/apache/poi/xssf/usermodel/examples/CreateTable.java
+++ src/examples/src/org/apache/poi/xssf/usermodel/examples/CreateTable.java
@@ -4,10 +4,27 @@
  */
 package org.apache.poi.xssf.usermodel.examples;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
+ * Demonstrates how to work with excel tables.
  *
- * @author s
+ * @author Sofia Larsson
  */
 public class CreateTable {
-    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet();
+     
+        FileOutputStream fileOut = new FileOutputStream("ooxml-table.xlsx");
+        
+        wb.write(fileOut);
+        fileOut.close();
+    }
 }
