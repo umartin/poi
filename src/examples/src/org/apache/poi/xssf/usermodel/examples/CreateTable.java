2f48fd45ac72225c12c68ed3548d4c5fab11fa82
karinsofia <karinsofiapaulina@gmail.com>
2013-09-16 16:03:55 +0200

    Add comments to table example

--- src/examples/src/org/apache/poi/xssf/usermodel/examples/CreateTable.java
+++ src/examples/src/org/apache/poi/xssf/usermodel/examples/CreateTable.java
@@ -31,35 +31,39 @@ public class CreateTable {
         Workbook wb = new XSSFWorkbook();
         XSSFSheet sheet = (XSSFSheet) wb.createSheet();
         
+        //Create 
         XSSFTable table = sheet.createTable();
-        table.setDisplayName("Test");
-               
+        table.setDisplayName("Test");       
         CTTable cttable = table.getCTTable();
+        
+        //Style configurations
         CTTableStyleInfo style = cttable.addNewTableStyleInfo();
         style.setName("TableStyleMedium2");
         style.setShowColumnStripes(false);
         style.setShowRowStripes(true);
-        style.setShowFirstColumn(false);
-        style.setShowLastColumn(false);
         
+        //Set which area the table should be placed in
         AreaReference reference = new AreaReference(new CellReference(0, 0), 
                 new CellReference(2,2));
         cttable.setRef(reference.formatAsString());
         cttable.setId(1);
         cttable.setName("Test");
         cttable.setTotalsRowCount(1);
-                
+        
         CTTableColumns columns = cttable.addNewTableColumns();
         columns.setCount(3);
         CTTableColumn column;
         XSSFRow row;
         XSSFCell cell;
         for(int i=0; i<3; i++) {
+            //Create column
             column = columns.addNewTableColumn();
             column.setName("Column");
             column.setId(i+1);
+            //Create row
             row = sheet.createRow(i);
             for(int j=0; j<3; j++) {
+                //Create cell
                 cell = row.createCell(j);
                 if(i == 0) {
                     cell.setCellValue("Column"+j);
