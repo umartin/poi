da0cda768889e1b29a802380e37849e579e9a6b2
karinsofia <karinsofiapaulina@gmail.com>
2013-09-26 16:05:14 +0200

    Test to create PivotCacheDefinitions

--- src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
+++ src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
@@ -3,10 +3,8 @@ package org.apache.poi.xssf.usermodel.examples;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFPivotTable;
-import org.apache.poi.xssf.usermodel.XSSFRelation;
+import org.apache.poi.xssf.usermodel.XSSFPivotCacheDefinition;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -19,11 +17,7 @@ public class CreatePivotTable {
         Workbook wb = new XSSFWorkbook();
         XSSFSheet sheet = (XSSFSheet) wb.createSheet();
         sheet.createPivotTable();
-/*
-        wb.addPivotCache();
-        wb.addPivotCache();
-        
-       */ 
+  
         FileOutputStream fileOut = new FileOutputStream("ooxml-pivottable.xlsx");
         wb.write(fileOut);
         fileOut.close(); 
