9d43822cd3dde099ac5e6ad7bb00fbd030f65929
karinsofia <karinsofiapaulina@gmail.com>
2013-09-26 14:10:53 +0200

    PivotTable extends POIXMLDocumentPart

--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotTable.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotTable.java
@@ -1,8 +1,19 @@
 package org.apache.poi.xssf.usermodel;
 
+import org.apache.poi.POIXMLDocumentPart;
+import org.apache.poi.openxml4j.opc.PackagePart;
+import org.apache.poi.openxml4j.opc.PackageRelationship;
+
 /**
  *
  */
-public class XSSFPivotTable {
+public class XSSFPivotTable extends POIXMLDocumentPart{
+    
+    public XSSFPivotTable() {
+    }
+    
+    public XSSFPivotTable(PackagePart part, PackageRelationship rel) {
+        super(part, rel);
+    }
     
 }
