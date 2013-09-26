479209cab2c7e35ac5e25435d199b2944176b6a5
karinsofia <karinsofiapaulina@gmail.com>
2013-09-26 14:07:57 +0200

    Example to try to create relation between pivotTable and sheet

--- src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
+++ src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
@@ -3,7 +3,11 @@ package org.apache.poi.xssf.usermodel.examples;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFPivotTable;
+import org.apache.poi.xssf.usermodel.XSSFRelation;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
@@ -13,13 +17,16 @@ public class CreatePivotTable {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Workbook wb = new XSSFWorkbook();
-        
+        XSSFSheet sheet = (XSSFSheet) wb.createSheet();
+        sheet.createPivotTable();
+/*
         wb.addPivotCache();
         wb.addPivotCache();
         
+       */ 
         FileOutputStream fileOut = new FileOutputStream("ooxml-pivottable.xlsx");
         wb.write(fileOut);
-        fileOut.close();
+        fileOut.close(); 
     }
 
 }
