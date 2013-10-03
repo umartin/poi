aa2022e9688b33f218ad486981485de65d389d79
karinsofia <karinsofiapaulina@gmail.com>
2013-10-03 09:33:00 +0200

    Create pivot cache records and add licenses

--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotCacheDefinition.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotCacheDefinition.java
@@ -14,18 +14,10 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 ==================================================================== */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.apache.poi.xssf.usermodel;
 
 import org.apache.poi.POIXMLDocumentPart;
 
-/**
- *
- * @author s
- */
 public class XSSFPivotCacheDefinition extends POIXMLDocumentPart{
     
 }
--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotCacheRecord.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFPivotCacheRecord.java
@@ -14,28 +14,10 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 ==================================================================== */
-package org.apache.poi.xssf.usermodel.examples;
+package org.apache.poi.xssf.usermodel;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.POIXMLDocumentPart;
 
-/**
- *
- */
-public class CreatePivotTable {
+public class XSSFPivotCacheRecord extends POIXMLDocumentPart {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        Workbook wb = new XSSFWorkbook();
-        XSSFSheet sheet = (XSSFSheet) wb.createSheet();
-        sheet.createPivotTable();
-  
-        FileOutputStream fileOut = new FileOutputStream("ooxml-pivottable.xlsx");
-        wb.write(fileOut);
-        fileOut.close(); 
-    }
-
 }
