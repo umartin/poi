33c2aabce9f649a6fe000c877b225acedf5b08e6
karinsofia <karinsofiapaulina@gmail.com>
2013-11-04 16:16:00 +0100

    Create pivot table example

--- src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
+++ src/examples/src/org/apache/poi/xssf/usermodel/examples/CreatePivotTable.java
@@ -29,14 +29,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.STDataConsolidateFunction;
 
-/**
- *
- */
 public class CreatePivotTable {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         createPivot("ooxml-pivottable.xlsx");
-        createPivot("ooxml-pivottable.zip");
     }
     
     public static void createPivot(String fileName)throws FileNotFoundException, IOException{
@@ -46,12 +42,17 @@ public class CreatePivotTable {
         //Create some data to build the pivot table on
         setCellData(sheet);
         
-        XSSFPivotTable pivotTable = sheet.createPivotTable(new AreaReference("A1:C3"), new CellReference("H5"));
+        XSSFPivotTable pivotTable = sheet.createPivotTable(new AreaReference("A1:D4"), new CellReference("H5"));
         //Configure the pivot table
+        //Use first column as row label
         pivotTable.addRowLabel(0);
-        pivotTable.addRowLabel(1);
-        pivotTable.addColumnLabel(STDataConsolidateFunction.SUM,2);
-        
+        //Sum up the second column
+        pivotTable.addColumnLabel(STDataConsolidateFunction.SUM, 1);
+        //Set the third column as filter
+        pivotTable.addColumnLabel(STDataConsolidateFunction.AVERAGE, 2);
+        //Add filter on forth column
+        pivotTable.addReportFilter(3);
+                
         FileOutputStream fileOut = new FileOutputStream(fileName);
         wb.write(fileOut);
         fileOut.close();
@@ -60,27 +61,43 @@ public class CreatePivotTable {
     public static void setCellData(XSSFSheet sheet){
         Row row1 = sheet.createRow((short) 0);
         // Create a cell and put a value in it.
-        Cell cell = row1.createCell((short) 0);
-        cell.setCellValue("Names");
-        Cell cell2 = row1.createCell((short) 1);
-        cell2.setCellValue("#");
-        Cell cell7 = row1.createCell((short) 2);
-        cell7.setCellValue("%");
+        Cell cell11 = row1.createCell((short) 0);
+        cell11.setCellValue("Names");
+        Cell cell12 = row1.createCell((short) 1);
+        cell12.setCellValue("#");
+        Cell cell13 = row1.createCell((short) 2);
+        cell13.setCellValue("%");
+        Cell cell14 = row1.createCell((short) 3);
+        cell14.setCellValue("Human");
 
         Row row2 = sheet.createRow((short) 1);
-        Cell cell3 = row2.createCell((short) 0);
-        cell3.setCellValue("Jessica");
-        Cell cell4 = row2.createCell((short) 1);
-        cell4.setCellValue(3);
-        Cell cell8 = row2.createCell((short) 2);
-        cell8.setCellValue(85);
+        Cell cell21 = row2.createCell((short) 0);
+        cell21.setCellValue("Jane");
+        Cell cell22 = row2.createCell((short) 1);
+        cell22.setCellValue(10);
+        Cell cell23 = row2.createCell((short) 2);
+        cell23.setCellValue(100);
+        Cell cell24 = row2.createCell((short) 3);
+        cell24.setCellValue("Yes");
 
         Row row3 = sheet.createRow((short) 2);
-        Cell cell5 = row3.createCell((short) 0);
-        cell5.setCellValue("Sofia");
-        Cell cell6 = row3.createCell((short) 1);
-        cell6.setCellValue(3);
-        Cell cell9 = row3.createCell((short) 2);
-        cell9.setCellValue(82);  
+        Cell cell31 = row3.createCell((short) 0);
+        cell31.setCellValue("Tarzan");
+        Cell cell32 = row3.createCell((short) 1);
+        cell32.setCellValue(5);
+        Cell cell33 = row3.createCell((short) 2);
+        cell33.setCellValue(90);  
+        Cell cell34 = row3.createCell((short) 3);
+        cell34.setCellValue("Yes");
+        
+        Row row4 = sheet.createRow((short) 3);
+        Cell cell41 = row4.createCell((short) 0);
+        cell41.setCellValue("Terk");
+        Cell cell42 = row4.createCell((short) 1);
+        cell42.setCellValue(10);
+        Cell cell43 = row4.createCell((short) 2);
+        cell43.setCellValue(90);  
+        Cell cell44 = row4.createCell((short) 3);
+        cell44.setCellValue("No");
     }
 }
